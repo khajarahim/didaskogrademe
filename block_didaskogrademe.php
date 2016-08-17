06a87c16b6831bdda12c618d6c99aae34aacb7bf
root <root@test01.didaskocloud.com>
2016-08-16 21:38:18 -0500

    Jquery added

--- block_didaskogrademe.php
+++ block_didaskogrademe.php
@@ -5,13 +5,16 @@ class block_didaskogrademe extends block_base{
 	}
 	
 	public function get_content() {
-		global $PAGE;
+		global $CFG, $PAGE;
 		// null checking
 		if ($this->content !== null) {
 			return $this->content;
 		}
 	
-		// inclide javascript 
+		// inclide javascript
+   		require_once($CFG->dirroot.'/blocks/grade_me/lib.php');
+		$PAGE->requires->jquery();
+		$PAGE->requires->js('/blocks/grade_me/javascript/grademe.js'); 
 		$PAGE->requires->js('/blocks/didaskogrademe/javascript/didaskogrademe.js');
 		$this->content         =  new stdClass;
 		$this->content->text = '<button id="didaskoGradeMeButton" class="btn btn-mini btn-primary" type="button">Collapse/Expand All</button>';
